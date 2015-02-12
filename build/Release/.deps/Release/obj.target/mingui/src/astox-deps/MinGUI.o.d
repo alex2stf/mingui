767535ad567640f6ccc3ac2a2eff19d91d6b056c
alex2stf <alex2stf@yahoo.com>
2015-02-12 10:43:40 +0200

    gtk support ended

--- build/Release/.deps/Release/obj.target/mingui/src/astox-deps/MinGUI.o.d
+++
@@ -1,5 +0,0 @@
-cmd_Release/obj.target/mingui/src/astox-deps/MinGUI.o := c++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DASTX_ENABLE_NODEJS' '-D__MACOSX_CORE__' '-DBUILDING_NODE_EXTENSION' -I/Users/alexandru/.node-gyp/0.10.28/src -I/Users/alexandru/.node-gyp/0.10.28/deps/uv/include -I/Users/alexandru/.node-gyp/0.10.28/deps/v8/include -I../src/astox-deps  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing  -MMD -MF ./Release/.deps/Release/obj.target/mingui/src/astox-deps/MinGUI.o.d.raw -c -o Release/obj.target/mingui/src/astox-deps/MinGUI.o ../src/astox-deps/MinGUI.mm
-Release/obj.target/mingui/src/astox-deps/MinGUI.o: \
-  ../src/astox-deps/MinGUI.mm ../src/astox-deps/MinGUI.h
-../src/astox-deps/MinGUI.mm:
-../src/astox-deps/MinGUI.h:
